93dba940cd28c2a0b0b57b8651b0a343fd6935e6
kei-g <km.8k6ce+github@gmail.com>
2023-09-11 20:41:09 +0900

    :sparkles: Support target language for translation
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- src/lib/deepl.ts
+++ src/lib/deepl.ts
@@ -2,6 +2,91 @@ import { DeepLResult, receiveAsJsonAsync } from '..'
 import { request as createRequest } from 'https'
 
 export namespace DeepL {
+  export class LanguageMap {
+    private readonly codes = new Map<string, string>()
+    private readonly names = new Map<string, string>()
+
+    constructor() {
+      for (const key in languages) {
+        const value = languages[key as keyof TypeOfLanguage]
+        this.codes.set(key, value)
+        this.names.set(value, key)
+      }
+    }
+
+    getCode(name: string): string {
+      return this.names.get(name)
+    }
+
+    getName(code: string): string {
+      return this.codes.get(code.toLowerCase())
+    }
+  }
+
+  type TypeOfLanguage = {
+    bg: 'ブルガリア語'
+    cs: 'チェコ語'
+    da: 'デンマーク語'
+    de: 'ドイツ語'
+    el: 'ギリシャ語'
+    en: '英語'
+    es: 'スペイン語'
+    et: 'エストニア語'
+    fi: 'フィンランド語'
+    fr: 'フランス語'
+    hu: 'ハンガリー語'
+    id: 'インドネシア語'
+    it: 'イタリア語'
+    ja: '日本語'
+    ko: '韓国語'
+    lt: 'リトアニア語'
+    lv: 'ラトビア語'
+    nb: 'ノルウェー語'
+    nl: 'オランダ語'
+    pl: 'ポーランド語'
+    pt: 'ポルトガル語'
+    ro: 'ルーマニア語'
+    ru: 'ロシア語'
+    sk: 'スロバキア語'
+    sl: 'スロベニア語'
+    sv: 'スウェーデン語'
+    tr: 'トルコ語'
+    uk: 'ウクライナ語'
+    zh: '中国語'
+  }
+
+  export const languages = {
+    bg: 'ブルガリア語',
+    cs: 'チェコ語',
+    da: 'デンマーク語',
+    de: 'ドイツ語',
+    el: 'ギリシャ語',
+    en: '英語',
+    es: 'スペイン語',
+    et: 'エストニア語',
+    fi: 'フィンランド語',
+    fr: 'フランス語',
+    hu: 'ハンガリー語',
+    id: 'インドネシア語',
+    it: 'イタリア語',
+    ja: '日本語',
+    ko: '韓国語',
+    lt: 'リトアニア語',
+    lv: 'ラトビア語',
+    nb: 'ノルウェー語',
+    nl: 'オランダ語',
+    pl: 'ポーランド語',
+    pt: 'ポルトガル語',
+    ro: 'ルーマニア語',
+    ru: 'ロシア語',
+    sk: 'スロバキア語',
+    sl: 'スロベニア語',
+    sv: 'スウェーデン語',
+    tr: 'トルコ語',
+    uk: 'ウクライナ語',
+    zh: '中国語',
+  } as TypeOfLanguage
+
   const mayBeAssumedAsEnglish = (text: string): boolean => {
     for (let i = 0; i < text.length; i++) {
       const c = text.charCodeAt(i)
