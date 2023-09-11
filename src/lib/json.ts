6c7897083aed221010fdc7cde50c41cee256abdb
kei-g <km.8k6ce+github@gmail.com>
2023-09-11 15:27:24 +0900

    :sparkles: Support translation by DeepL
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

---
+++ src/lib/json.ts
@@ -0,0 +1,14 @@
+import { ClientRequest, IncomingMessage } from 'http'
+import { bind1st } from '..'
+
+const doReceive = <T>(resolve: (value: T) => void, response: IncomingMessage) => {
+  const list = [] as Buffer[]
+  response.on('data', list.push.bind(list))
+  response.on('end', () => resolve(JSON.parse(Buffer.concat(list).toString()) as T))
+}
+
+export const receiveAsJsonAsync = <T>(request: ClientRequest, content: Buffer): Promise<T> => {
+  const task = new Promise((resolve: (value: T) => void) => request.on('response', bind1st(resolve, doReceive)))
+  request.write(content)
+  return task
+}
