b75d96d3f3d2362e3a703748e591c003ffdc195b
kei-g <km.8k6ce+github@gmail.com>
2025-11-18 10:24:07 +0900

    :lipstick: Fix indent style
    Signed-off-by: kei-g <km.8k6ce+github@gmail.com>

--- phi.py
+++ phi.py
@@ -14,18 +14,18 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 torch.set_default_device("cpu")
 
 model = AutoModelForCausalLM.from_pretrained(
-    "microsoft/phi-1_5", trust_remote_code=True)
+	"microsoft/phi-1_5", trust_remote_code=True)
 tokenizer = AutoTokenizer.from_pretrained(
-    "microsoft/phi-1_5", trust_remote_code=True)
+	"microsoft/phi-1_5", trust_remote_code=True)
 
 print("ready", end="", flush=True)
 
 while True:
-    lines = sys.stdin.readline().splitlines()
-    if len(lines) == 0:
-        break
-    text = lines[0]
-    inputs = tokenizer(text, return_tensors="pt", return_attention_mask=False)
-    length = maxLength + len(text.split())
-    outputs = model.generate(**inputs, max_length=length)
-    print(tokenizer.batch_decode(outputs)[0], end="", flush=True)
+	lines = sys.stdin.readline().splitlines()
+	if len(lines) == 0:
+		break
+	text = lines[0]
+	inputs = tokenizer(text, return_tensors="pt", return_attention_mask=False)
+	length = maxLength + len(text.split())
+	outputs = model.generate(**inputs, max_length=length)
+	print(tokenizer.batch_decode(outputs)[0], end="", flush=True)
